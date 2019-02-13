b6e486e9be9b5018eeadcb63eab0f7bbe1cde82e
C3NZ <cenz@cenz.io>
2019-02-13 15:16:59 -0800

    [client:feat] setup the react dom router for our react app

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,26 +1,33 @@
+// React imports 
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import '../assets/css/App.css';
 
+const routes = [
+    {
+        path: '/',
+        component: 'x'
+    }
+]
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    registerRoutes() {
+
+    }
+    render() {
+        return (
+        <BrowserRouter>
+            <div className="App">
+                {routes.map((route, i) => (
+                    <Route
+                        key={1}>
+                    </Route>
+                ))} 
+            </div>
+        </BrowserRouter>
+        );
+    }
 }
 
 export default App;
