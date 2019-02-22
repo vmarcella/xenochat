d2f1238253878bf03ad175561e4f7b2b2636d623
C3NZ <cenz@cenz.io>
2019-02-21 21:55:41 -0800

    [server:fix] remove jade templating

--- server/app.js
+++ server/app.js
@@ -10,14 +10,12 @@ const db = require('./data/xeno-db');
 // Connect to db
 db.connect('mongodb://localhost/xenodb');
 
+// Register custom routes
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
 
 const app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // Register middleware
 app.use(logger('dev'));
@@ -29,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Register routes for endpoints
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
