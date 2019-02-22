a169d9746348b1ff923faec009fc86b65afdf027
C3NZ <cenz@cenz.io>
2019-02-21 19:57:26 -0800

    [server:feat] update model to include password hashing

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const { Schema } = mongoose;
 
 // Create the user schema
+// timestamps will keep track of the updated and created at tags
 const userSchema = new Schema({
     username: {
         type: String,
@@ -15,7 +17,35 @@ const userSchema = new Schema({
     },
     password: {
         type: String,
+        select: false,
     },
+},
+{
+    timestamps: true,
 });
+
+// handle when the user has updated their password
+userSchema.pre('save', function(next) {
+    const user = this;
+
+    if (user.isModified('password')) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (error, hash) => {
+                if (err) next(err);
+
+                user.password = hash;
+                return next();
+            });
+        })
+    }
+});
+
+// Create a function which will allow us to securely compare passwords
+// of users that are stored within the database
+userSchema.methods.comparePassword = function(password, done) {
+    bcrypt.compare(password, this.password, (err, isMatch) => {
+        done(err, isMatch);
+    });
+}
 
 module.exports = mongoose.model('User', userSchema);
