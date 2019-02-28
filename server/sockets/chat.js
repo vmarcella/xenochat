6b2a8fd0cf472747014807a150009c9bba6704e9
C3NZ <cenz@cenz.io>
2019-02-27 16:25:56 -0800

    [server:feat] add remaining socket events

--- server/sockets/chat.js
+++ server/sockets/chat.js
@@ -12,6 +12,38 @@ const handleChat = (io, socket, serverInfo) => {
         // Alert all users that someone has entered the chat room
         io.emit('new user', user.username);
     });
+
+    // When a new message has been sent by a user 
+    socket.on('new message', (data) => {
+        console.log(`${data.sender}: ${data.message}`);
+        io.to(data.channel).emit('new message', data);
+    });
+
+    // When a new channel has been created
+    socket.on('new channel', (newChannel) => {
+        serverInfo.channels[newChannel] = [];
+        socket.join(newChannel);
+
+        io.emit('new channel', newChannel);
+
+        const channelData = {
+            channel: newChannel,
+            messages: serverInfo.channels[newChannel],
+        };
+
+        socket.emit('user changed channel', channelData);
+    });
+
+    // When a client wants to get all online users
+    socket.on('get online users', () => {
+        socket.emit('get online users', onlineUsers);
+    });
+
+    // When the client disconnects
+    socket.on('disconnect', () => {
+        delete serverInfo.onlineUsers[socket.username];
+        io.emit('user has left', onlineUsers);
+    });
 }
 
 // Export the handle chat wrapper function
