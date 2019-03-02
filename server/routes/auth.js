56d62159b24187aadbbd39ba6e0a9c1f4040b277
C3NZ <cenz@cenz.io>
2019-03-02 01:30:21 -0800

    [server:feat] fix login route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -32,4 +32,47 @@ router.post('/signup', async (req, res, next) => {
     }
 });
 
+// Log the user into the chat server
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username }).select('username password');
+
+        // Compare the password against the one the user has entered
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (err) {
+                return res.json({
+                    err: 'db error',
+                    body: 'Something went wrong with the database, the owner has been contacted!',
+                });
+            }
+
+            if (isMatch) {
+                const token = jwt.sign({
+                    _id: user._id,
+                    username: user.username,
+                }, process.env.SECRET, {
+                    expiresIn: '60 days',
+                });
+
+                // What to send the user for storage
+                const xenoUser = {
+                    username: user.username,
+                    _id: user._id,
+                    token,
+                };
+
+                return res.json(xenoUser);
+            }
+
+            // Passwords dont match, error
+            return res.json({
+                err: 'pword mismatch',
+                body: 'Passwords did not match',
+            });
+        })
+    } catch (err) {
+        res.json(err);
+    }
+});
+
 module.exports = router;
