b04a17bc8283811acddf054fa8bba4ea77a2ea80
C3NZ <cenz@cenz.io>
2019-03-03 21:01:02 -0800

    [server:feat] add mailgrid for sending emails to our lovely users

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,19 +1,22 @@
+// Import npm modules
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const sgMail = require('@sendgrid/mail');
+
+// Import the user model
 const User = require('../models/User');
 
 const router = express.Router();
-
-const maxAge = 60 * 24 * 60 * 60 * 1000;
+sgMail.setApiKey(process.env.SG_API_KEY);
 
 // Sign up route
 router.post('/signup', async (req, res, next) => {
     const user = new User(req.body);
-    
+
     try {
         const savedUser = await user.save();
-        const token = jwt.sign({ 
-            _id: savedUser._id, 
+        const token = jwt.sign({
+            _id: savedUser._id,
             username: user.username,
         }, process.env.SECRET, {
             expiresIn: '60 days',
@@ -23,11 +26,24 @@ router.post('/signup', async (req, res, next) => {
         const xenoUser = {
             username: user.username,
             _id: savedUser._id,
-            token
-        }
+            token,
+        };
+
+        const mail = {
+            from: process.env.EMAIL,
+            to: user.email,
+            subject: 'Thanks for signing up for xenochat',
+            text: `
+            Weclome to xenochat, ${user.username}! We hope that you enjoy the
+            chatroom we continue to build out and look forward to any feedback you
+            may have.
+            `,
+        };
+
+        sgMail.send(mail);
 
         return res.json(xenoUser);
-    } catch(err){
+    } catch (err) {
         next(err);
     }
 });
@@ -61,6 +77,14 @@ router.post('/login', async (req, res) => {
                     token,
                 };
 
+                const mail = {
+                    from: process.env.EMAIL,
+                    to: user.email,
+                    subject: 'Sign in',
+                    text: `We just received a sign in for your account: ${user.username}
+                        if this was you, then you can ignore this message 
+                    `
+                }
                 return res.json(xenoUser);
             }
 
@@ -69,7 +93,7 @@ router.post('/login', async (req, res) => {
                 err: 'pword mismatch',
                 body: 'Passwords did not match',
             });
-        })
+        });
     } catch (err) {
         res.json(err);
     }
